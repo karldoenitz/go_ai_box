bf151bb8cfe884d0d9b31e10e6aa0943d0595627
karl <karlvorndoenitz@gmail.com>
2018-03-14 17:33:08 +0800

    增加handler和redis client

--- handlers/LeanHandlers.go
+++ handlers/LeanHandlers.go
@@ -4,17 +4,43 @@ import (
 	"net/http"
 	"fmt"
 	"../utils"
+	"../global"
 )
 
-// 返回首页数据，主要是页面的UI布局数据
+// 返回首页UI数据
 func HomeHandler(responseWriter http.ResponseWriter, request *http.Request)  {
+	response := utils.Response{}
 	isValid, err := utils.IsParamsValid(request)
 	if !isValid {
-		response := utils.Response{Message:err}
-		fmt.Fprintf(responseWriter, response.ParamInvalid())
+		fmt.Fprintf(responseWriter, response.ParamInvalid(err))
 		return
 	}
 	level := request.Form.Get("level")
 	platform := request.Form.Get("ptf")
 	redisKey := fmt.Sprintf(utils.LeanKey, platform, level)
+	result, redisErr := global.RedisClient.Get(redisKey).Result()
+	if redisErr != nil {
+		fmt.Fprintf(responseWriter, response.ServerError(redisErr.Error()))
+		return
+	}
+	fmt.Fprintf(responseWriter, response.Success(result))
+}
+
+// 返回详情页UI数据
+func DetailHandler(responseWriter http.ResponseWriter, request *http.Request)  {
+	response := utils.Response{}
+	isValid, err := utils.IsParamsValid(request)
+	if !isValid {
+		fmt.Fprintf(responseWriter, response.ParamInvalid(err))
+		return
+	}
+	level := request.Form.Get("level")
+	platform := request.Form.Get("ptf")
+	redisKey := fmt.Sprintf(utils.DetailKey, platform, level)
+	result, redisErr := global.RedisClient.Get(redisKey).Result()
+	if redisErr != nil {
+		fmt.Fprintf(responseWriter, response.ServerError(redisErr.Error()))
+		return
+	}
+	fmt.Fprintf(responseWriter, response.Success(result))
 }
--- main/main.go
+++ main/main.go
@@ -3,14 +3,34 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"github.com/go-redis/redis"
 	"../handlers"
+	"../utils"
+	"../global"
 )
 
+func RedisFactory() (client *redis.Client, err error) {
+	client = redis.NewClient(&redis.Options{
+		Addr:       utils.RedisServer,
+		Password:   utils.RedisPwd,
+		DB:         utils.DB,
+	})
+	_, err = client.Ping().Result()
+	return client, err
+}
+
 func main()  {
+	fmt.Printf("redis connectting ...")
+	client, redisClientErr := RedisFactory()
+	if redisClientErr != nil {
+		fmt.Printf("redis connect failed: %s", client)
+		return
+	}
+	global.RedisClient = client
 	fmt.Printf("Server is running ...")
 	http.HandleFunc("/lean/home", handlers.HomeHandler)
-	err := http.ListenAndServe("127.0.0.1:8908", nil)
-	if err != nil {
-		fmt.Printf("error is: %s", err)
+	httpServerErr := http.ListenAndServe("127.0.0.1:8908", nil)
+	if httpServerErr != nil {
+		fmt.Printf("error is: %s", httpServerErr)
 	}
 }
