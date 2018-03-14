b99df951bc896d033afbd8406046a947ff030550
karl <karlvorndoenitz@gmail.com>
2018-03-14 17:32:30 +0800

    新增model

--- models/models.go
+++ models/models.go
@@ -1,9 +1,24 @@
 package models
 
 type Music struct {
-
+	id int64
+	name string
+	singer int64
+	searchWorld []string
+	url string
 }
 
 type Playbill struct {
+	id int64
+	name string
+	searchWorld []string
+	music []int64
+}
 
+type Player struct {
+	id int64
+	name string
+	searchWorld []string
+	music []int64
+	playbill []int64
 }
