b8b72d0b101d53c3925ce0c2a1458edf4786e7c0
karl <karlvorndoenitz@gmail.com>
2018-03-26 19:22:35 +0800

    完成music handler

--- handlers/SourceHandlers.go
+++ handlers/SourceHandlers.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"fmt"
+	"encoding/json"
 	"net/http"
 	"../logger"
 	"../utils"
+	"../models"
+	"strconv"
 )
 
 // 音乐查询接口，从solr中查询Music数据
@@ -18,6 +21,19 @@ func MusicHandler(responseWriter http.ResponseWriter, request *http.Request)  {
 		return
 	}
 	// 此处是业务逻辑
+	id := request.Form.Get("id")
+	musicId, _ := strconv.Atoi(id)
+	musicSearchResults := models.GetMusicById(musicId)
+	musicData := models.MusicData{
+		Data: musicSearchResults,
+		Length: len(musicSearchResults),
+	}
+	jsonResult, jsonErr := json.Marshal(musicData)
+	if jsonErr != nil {
+		logger.Error.Println("marshal musicData to json Error, on ServiceHandlers.go line 31: %s", jsonErr.Error())
+		panic(jsonErr)
+	}
+	fmt.Fprintf(responseWriter, response.Success(string(jsonResult)))
 }
 
 // 播单查询接口，从solr中查询Playbill数据
--- models/models.go
+++ models/models.go
@@ -92,3 +92,147 @@ func GetMusicByName(name string) (result []Music) {
 	}
 	return result
 }
+
+func GetMusicById(id int) (result []Music) {
+	solrMusicConn := global.SolrClientMusic
+	fq := fmt.Sprintf("id:%d", id)
+	q := solr.Query{
+		Params: solr.URLParamMap{
+			"q": []string{"*:*"},
+			"fq": []string{fq},
+			"wt": []string{"json"},
+			"fl": []string{"*"},
+		},
+		Rows: 10,
+		Sort: "id+ASC",
+	}
+	queryString := q.String()
+	fmt.Println(queryString)
+	res, selectErr := solrMusicConn.Select(&q)
+	if selectErr != nil {
+		logger.Error.Printf("select ERROR in model.go line 111: %s", selectErr.Error())
+		panic(selectErr)
+	}
+	solrResults := res.Results
+	for i := 0; i < solrResults.NumFound; i++ {
+		collection := solrResults.Collection[i]
+		id := int64(collection.Fields["id"].(float64))
+		name := collection.Fields["name"].(string)
+		url := collection.Fields["url"].(string)
+		singerInterface := collection.Fields["singer"].([]interface{})
+		searchWordInterface := collection.Fields["searchWord"].([]interface{})
+		singer := make([]int64, len(singerInterface))
+		for i := range singerInterface {
+			singer[i] = int64(singerInterface[i].(float64))
+		}
+		searchWord := make([]string, len(searchWordInterface))
+		for i := range searchWordInterface {
+			searchWord[i] = searchWordInterface[i].(string)
+		}
+		musicObj := Music{
+			Id: id,
+			Name: name,
+			Url: url,
+			Singer: singer,
+			SearchWorld: searchWord,
+		}
+		result = append(result, musicObj)
+	}
+	return result
+}
+
+func GetSingerById(id int) (result []Music) {
+	solrMusicConn := global.SolrClientSinger
+	fq := fmt.Sprintf("id:%d", id)
+	q := solr.Query{
+		Params: solr.URLParamMap{
+			"q": []string{"*:*"},
+			"fq": []string{fq},
+			"wt": []string{"json"},
+			"fl": []string{"*"},
+		},
+		Rows: 10,
+		Sort: "id+ASC",
+	}
+	queryString := q.String()
+	fmt.Println(queryString)
+	res, selectErr := solrMusicConn.Select(&q)
+	if selectErr != nil {
+		logger.Error.Printf("select ERROR in model.go line 159: %s", selectErr.Error())
+		panic(selectErr)
+	}
+	solrResults := res.Results
+	for i := 0; i < solrResults.NumFound; i++ {
+		collection := solrResults.Collection[i]
+		id := int64(collection.Fields["id"].(float64))
+		name := collection.Fields["name"].(string)
+		url := collection.Fields["url"].(string)
+		singerInterface := collection.Fields["singer"].([]interface{})
+		searchWordInterface := collection.Fields["searchWord"].([]interface{})
+		singer := make([]int64, len(singerInterface))
+		for i := range singerInterface {
+			singer[i] = int64(singerInterface[i].(float64))
+		}
+		searchWord := make([]string, len(searchWordInterface))
+		for i := range searchWordInterface {
+			searchWord[i] = searchWordInterface[i].(string)
+		}
+		musicObj := Music{
+			Id: id,
+			Name: name,
+			Url: url,
+			Singer: singer,
+			SearchWorld: searchWord,
+		}
+		result = append(result, musicObj)
+	}
+	return result
+}
+
+func GetPlaybillById(id int) (result []Music) {
+	solrMusicConn := global.SolrClientMusic
+	fq := fmt.Sprintf("id:%d", id)
+	q := solr.Query{
+		Params: solr.URLParamMap{
+			"q": []string{"*:*"},
+			"fq": []string{fq},
+			"wt": []string{"json"},
+			"fl": []string{"*"},
+		},
+		Rows: 10,
+		Sort: "id+ASC",
+	}
+	queryString := q.String()
+	fmt.Println(queryString)
+	res, selectErr := solrMusicConn.Select(&q)
+	if selectErr != nil {
+		logger.Error.Printf("select ERROR in model.go line 63: %s", selectErr.Error())
+		panic(selectErr)
+	}
+	solrResults := res.Results
+	for i := 0; i < solrResults.NumFound; i++ {
+		collection := solrResults.Collection[i]
+		id := int64(collection.Fields["id"].(float64))
+		name := collection.Fields["name"].(string)
+		url := collection.Fields["url"].(string)
+		singerInterface := collection.Fields["singer"].([]interface{})
+		searchWordInterface := collection.Fields["searchWord"].([]interface{})
+		singer := make([]int64, len(singerInterface))
+		for i := range singerInterface {
+			singer[i] = int64(singerInterface[i].(float64))
+		}
+		searchWord := make([]string, len(searchWordInterface))
+		for i := range searchWordInterface {
+			searchWord[i] = searchWordInterface[i].(string)
+		}
+		musicObj := Music{
+			Id: id,
+			Name: name,
+			Url: url,
+			Singer: singer,
+			SearchWorld: searchWord,
+		}
+		result = append(result, musicObj)
+	}
+	return result
+}
