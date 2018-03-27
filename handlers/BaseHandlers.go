7615cb58cf68671160f099f1bf34a5f7f4eaccb2
karl <karlvorndoenitz@gmail.com>
2018-03-27 19:30:35 +0800

    调整代码结构

--- handlers/BaseHandlers.go
+++ handlers/BaseHandlers.go
@@ -4,22 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"../utils"
+	"../logger"
 )
 
 func ResponseAsJson(responseWriter http.ResponseWriter, result string) {
 	response := utils.Response{}
 	responseWriter.Header().Set("Content-Type", "application/json")
+	logger.Info.Println("Response Success ...")
 	fmt.Fprintf(responseWriter, response.Success(result))
 }
 
 func ResponseParamInvalid(responseWriter http.ResponseWriter, err string)  {
 	response := utils.Response{}
 	responseWriter.Header().Set("Content-Type", "application/json")
+	logger.Warning.Println(err)
 	fmt.Fprintf(responseWriter, response.ParamInvalid(err))
 }
 
 func ResponseServerErr(responseWriter http.ResponseWriter, err string)  {
 	response := utils.Response{}
 	responseWriter.Header().Set("Content-Type", "application/json")
+	logger.Error.Println(err)
 	fmt.Fprintf(responseWriter, response.ServerError(err))
 }
