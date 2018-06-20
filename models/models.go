0512b658962d4fe818a27db9e75ff92d68a3349a
karl <karlvorndoenitz@gmail.com>
2018-06-20 17:56:10 +0800

    增加了json标识

--- models/models.go
+++ models/models.go
@@ -1,41 +1,41 @@
 package models
 
 type MusicData struct {
-	Data []Music
-	Length int
+	Data      []Music     `json:"data"`
+	Length    int         `json:"length"`
 }
 
 type PlaybillData struct {
-	Data []Playbill
-	Length int
+	Data      []Playbill  `json:"data"`
+	Length    int         `json:"length"`
 }
 
 type SingerData struct {
-	Data []Singer
-	Length int
+	Data      []Singer    `json:"data"`
+	Length    int         `json:"length"`
 }
 
 type Music struct {
-	Id int64
-	Name string
-	Singer []int64
-	SearchWorld []string
-	Url string
+	Id          int64      `json:"id"`
+	Name        string     `json:"name"`
+	Singer      []int64    `json:"singer"`
+	SearchWorld []string   `json:"search_world"`
+	Url         string     `json:"url"`
 }
 
 type Playbill struct {
-	Id int64
-	Name string
-	SearchWorld []string
-	Music []int64
+	Id          int64      `json:"id"`
+	Name        string     `json:"name"`
+	SearchWorld []string   `json:"search_world"`
+	Music       []int64    `json:"music"`
 }
 
 type Singer struct {
-	Id int64
-	Name string
-	SearchWorld []string
-	Music []int64
-	Playbill []int64
+	Id int64               `json:"id"`
+	Name string            `json:"name"`
+	SearchWorld []string   `json:"search_world"`
+	Music []int64          `json:"music"`
+	Playbill []int64       `json:"playbill"`
 }
 
 
